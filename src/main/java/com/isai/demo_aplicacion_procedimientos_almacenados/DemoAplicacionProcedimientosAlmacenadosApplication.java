a6a8ea8965ff456d3af52630842c96ee4f2aa233
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-26 10:55:41 -0500

    init

---
+++ src/main/java/com/isai/demo_aplicacion_procedimientos_almacenados/DemoAplicacionProcedimientosAlmacenadosApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_aplicacion_procedimientos_almacenados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAplicacionProcedimientosAlmacenadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAplicacionProcedimientosAlmacenadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_aplicacion_procedimientos_almacenados/DemoAplicacionProcedimientosAlmacenadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_aplicacion_procedimientos_almacenados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAplicacionProcedimientosAlmacenadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
