2cb8a6e3b875b7f1fdb48802bf87d50d1b9867f7
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-26 11:18:27 -0500

    archivo import.sql con datos para la tabla departamentos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+-- src/main/resources/import.sql
+-- Datos iniciales para la tabla departamento
+
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (1, 35000.00, 'Recursos Humanos', 50000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (2, 95000.00, 'Tecnología', 120000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (3, 60000.00, 'Marketing', 75000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (4, 185000.00, 'Producción', 200000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (5, 82000.00, 'Ventas', 90000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (6, 135000.00, 'I+D', 150000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (7, 38000.00, 'Administración', 40000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (8, 55000.00, 'Desarrollo', 60000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (9, 80000.00, 'Finanzas', 85000.00);
+INSERT INTO departamento (id_departamento, gastos_departamento, nombre_departamento, presupuesto_departamento) VALUES (10, 105000.00, 'Operaciones', 110000.00);
